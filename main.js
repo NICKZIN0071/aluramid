d47ce3e54fd53f6d7c7c50f23f7588cce671acea
NICKZIN0071 <129960230+NICKZIN0071@users.noreply.github.com>
2023-11-01 11:01:37 -0300

    bunda

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 function tocaSomPom(){
 document.querySelector('#som_tecla_pom').play('');
 }
-document.queryselector( '.tecla_pom');
 
+
+if(elemento !=null && elemento.localName === 'audio'){
+    elemento.play();
+}else{
+    alert('Elemento não encontrado');
+}
+const lista = document.querySelectorAll('tecla');
+
+for(contador = 0; contador<lista.length;contador++)
+{
+       lista  [contador].classlist[1];
+
+       const instrumento = lista [contador];
